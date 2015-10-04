4c164864f80235dcedba332e70f964c4e35d35c6
Pierre Thalamy <pierre.thalamy@gmail.com>
2015-10-04 13:55:40 +0200

    Generated Makefile w/ CMake

---
+++ TP1-Allocateur/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /user/9/thalamyp/Cours/2A/SEPC/TP-SEPC-thalamyp-lerouxb/TP1-Allocateur
+# Build directory: /user/9/thalamyp/Cours/2A/SEPC/TP-SEPC-thalamyp-lerouxb/TP1-Allocateur
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(AllTestsAllocator "alloctest")
---
+++ TP1-Allocateur/src/variante.h
@@ -0,0 +1,7 @@
+#ifndef VARIANTE_H
+#define VARIANTE_H
+
+#define LOGINS lerouxb;thalamyp
+#define SUJET 1
+
+#endif
