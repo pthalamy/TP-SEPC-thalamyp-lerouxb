df8729bad1a6b9a2162bc13edb39ac4c1f742f92
Pierre Thalamy <pierre.thalamy@gmail.com>
2015-10-06 15:26:15 +0200

    Commentaire contenu des fonctions + gitignore

--- TP1-Allocateur/src/mem.c
+++ TP1-Allocateur/src/mem.c
@@ -17,48 +17,56 @@ void *zone_memoire = 0;
 /* Blocs de la forme (suivant, ...) */
 typedef struct _BlocZL {
     struct _BlocZL *suivant;
-    /* ??? */
+    void *addr;
 } BlocZL;
 /* TZL sous forme de tableau de blocs? */
 BlocZL *TZL;
 
-int
-mem_init()
+int mem_init()
 {
-  if (! zone_memoire)
-    zone_memoire = (void *) malloc(ALLOC_MEM_SIZE);
-  if (zone_memoire == 0)
-    {
-      perror("mem_init:");
-      return -1;
+    if (! zone_memoire)
+	zone_memoire = (void *) malloc(ALLOC_MEM_SIZE);
+    if (zone_memoire == 0) {
+	perror("mem_init:");
+	return -1;
     }
 
-  /* ecrire votre code ici */
+    /* ecrire votre code ici */
+    /* Allocation de la table de TZL de taille log2(ALLOC_MEM_SIZE) + 1 */
+    /* TZL vide sauf deniere case, bloc memoire complet */
 
-  return 0;
+    return 0;
 }
 
-void *
-mem_alloc(unsigned long size)
+void *mem_alloc(unsigned long size)
 {
-  /*  ecrire votre code ici */
-  return 0;
+    /*  ecrire votre code ici */
+    /* Check que size > 0 */
+    /* Regarde si TZL[log2(size - 1) + 1] comprend un bloc libre  */
+    /* Si oui, retire de la tzl et retourne l'@ associée */
+    /* Sinon, recherche récursive d'un bloc d'ordre supérieur à diviser */
+
+    return 0;
 }
 
-int
-mem_free(void *ptr, unsigned long size)
+int mem_free(void *ptr, unsigned long size)
 {
-  /* ecrire votre code ici */
-  return 0;
+    /* ecrire votre code ici */
+    /* xor entre @ et log2(size) + ? afin de trouver le buddy */
+    /* Si present dans TZL, fusion jusqu'ā ce que le bloc atteigne la taille MAX */
+    /*                      ou qu'un buddy manque */
+    /* Sinon, on se contente d'ajouter le bloc libéré à la TZL */
+
+    return 0;
 }
 
 
-int
-mem_destroy()
+int mem_destroy()
 {
-  /* ecrire votre code ici */
+    /* ecrire votre code ici */
+    /* Free des cellules des listes de la TZL, puis free de la TZL */
 
-  free(zone_memoire);
-  zone_memoire = 0;
-  return 0;
+    free(zone_memoire);
+    zone_memoire = 0;
+    return 0;
 }
