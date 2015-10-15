d94be173d3f494ac73a9071dc51953171c675885
Pierre Thalamy <pierre.thalamy@gmail.com>
2015-10-15 14:08:17 +0200

    Fetch latest update from gmounie's repository

--- TP2-Shell/src/ensishell.c
+++ TP2-Shell/src/ensishell.c
@@ -1,5 +1,5 @@
 /*****************************************************
- * Copyright Grégory Mounié 2008-2013                *
+ * Copyright Grégory Mounié 2008-2015                *
  *           Simon Nieuviarts 2002-2009              *
  * This code is distributed under the GLPv3 licence. *
  * Ce code est distribué sous la licence GPLv3+.     *
@@ -15,22 +15,99 @@
 #error "Variante non défini !!"
 #endif
 
+/* Guile (1.8 and 2.0) is auto-detected by cmake */
+/* To disable Scheme interpreter (Guile support), comment the
+ * following lines.  You may also have to comment related pkg-config
+ * lines in CMakeLists.txt.
+ */
+
+#if USE_GUILE == 1
+#include <libguile.h>
+
+int executer(char *line)
+{
+	/* Insert your code to execute the command line
+	 * identically to the standard execution scheme:
+	 * parsecmd, then fork+execvp, for a single command.
+	 * pipe and i/o redirection are not required.
+	 */
+	printf("Not implemented: can not execute %s\n", line);
+
+	/* Remove this line when using parsecmd as it will free it */
+	free(line);
+	
+	return 0;
+}
+
+SCM executer_wrapper(SCM x)
+{
+        return scm_from_int(executer(scm_to_locale_stringn(x, 0)));
+}
+#endif
+
+
+void terminate(char *line) {
+#ifdef USE_GNU_READLINE
+	/* rl_clear_history() does not exist yet in centOS 6 */
+	clear_history();
+#endif
+	if (line)
+	  free(line);
+	printf("exit\n");
+	exit(0);
+}
+
+
 int main() {
         printf("Variante %d: %s\n", VARIANTE, VARIANTE_STRING);
 
+#ifdef USE_GUILE
+        scm_init_guile();
+        /* register "executer" function in scheme */
+        scm_c_define_gsubr("executer", 1, 0, 0, executer_wrapper);
+#endif
+
 	while (1) {
 		struct cmdline *l;
+		char *line=0;
 		int i, j;
 		char *prompt = "ensishell>";
 
-		l = readcmd(prompt);
+		/* Readline use some internal memory structure that
+		   can not be cleaned at the end of the program. Thus
+		   one memory leak per command seems unavoidable yet */
+		line = readline(prompt);
+		if (line == 0 || ! strncmp(line,"exit", 4)) {
+			terminate(line);
+		}
+
+#ifdef USE_GNU_READLINE
+		add_history(line);
+#endif
+
+
+#ifdef USE_GUILE
+		/* The line is a scheme command */
+		if (line[0] == '(') {
+			char catchligne[strlen(line) + 256];
+			sprintf(catchligne, "(catch #t (lambda () %s) (lambda (key . parameters) (display \"mauvaise expression/bug en scheme\n\")))", line);
+			scm_eval_string(scm_from_locale_string(catchligne));
+			free(line);
+                        continue;
+                }
+#endif
+
+		/* parsecmd free line and set it up to 0 */
+		l = parsecmd( & line);
 
 		/* If input stream closed, normal termination */
 		if (!l) {
-			printf("exit\n");
-			exit(0);
+		  
+			terminate(0);
 		}
+		
 
+		
 		if (l->err) {
 			/* Syntax error, read another command */
 			printf("error: %s\n", l->err);
@@ -51,4 +128,5 @@ int main() {
 			printf("\n");
 		}
 	}
+
 }
