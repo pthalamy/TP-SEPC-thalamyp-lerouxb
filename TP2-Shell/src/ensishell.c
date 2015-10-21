55ca66fdc1f4d20d1a2347f8602418bfbd3d9179
Pierre Thalamy <pierre.thalamy@gmail.com>
2015-10-21 19:48:54 +0200

    parsing and execution of a line now done within function, scheme execution thus implemented

--- TP2-Shell/src/ensishell.c
+++ TP2-Shell/src/ensishell.c
@@ -47,6 +47,8 @@ shell_history BACKGROUND_PID = {NULL};
 /*     BACKGROUND_PID->head = NULL; */
 /* } */
 
+int parse_and_execute_line(char **line);
+
 void insert_shell_cmd(shell_history *liste, shell_cmd *command) {
 
     if (liste->head == NULL) {
@@ -87,7 +89,7 @@ int executer(char *line)
      * pipe and i/o redirection are not required.
      */
 
-    return 0;
+    return parse_and_execute_line(&line);
 }
 
 SCM executer_wrapper(SCM x)
@@ -106,6 +108,162 @@ void terminate(char *line) {
 	free(line);
     printf("exit\n");
     exit(0);
+}
+
+
+int parse_and_execute_line(char **line)
+{
+    /* Parse line */
+    struct cmdline *cl = parsecmd(line);
+
+    /* If input stream closed, normal termination */
+    if (!cl) {
+	terminate(0);
+    }
+
+    if (cl->err) {
+	/* Syntax error, read another command */
+	printf("error: %s\n", cl->err);
+	return -1;
+    } else if (!cl->seq[0])	/* Empty line */
+	return 0;
+
+#if DEBUG_COMMAND_LINE
+    if (cl->in) printf("in: %s\n", cl->in);
+    if (cl->out) printf("out: %s\n", cl->out);
+    if (cl->bg) printf("background (&)\n");
+
+    /* Display each command of the pipe */
+    for (int i = 0; cl->seq[i] != 0; i++) {
+	char **cmd = cl->seq[i];
+	printf("seq[%d]: ", i);
+	for (int j = 0; cmd[j] != 0; j++) {
+	    printf("'%s' ", cmd[j]);
+	}
+	printf("\n");
+    }
+#endif
+
+    int fds[2];			/* A pipe file-descriptor */
+    pid_t PID;
+
+    int in = 0;		/* input file desc: stdin by default */
+    if (cl->in)
+	in = open(cl->in, O_RDONLY);
+    int out = 1;		/* output file desc: stdout by default */
+    if (cl->out)
+	out = open(cl->out, O_WRONLY | O_CREAT);
+
+    /* If first command is "jobs" print PID */
+    if (!strncmp(cl->seq[0][0], "jobs", 4)) {
+	if (BACKGROUND_PID.head == NULL)
+	    printf("No background processes\n");
+	else {
+	    shell_cmd *cour = BACKGROUND_PID.head;
+	    printf("Active background processes:\n");
+	    int j = 1;
+	    while (cour != NULL) {
+		printf("+ [%d] PID: %d ", j,
+		       cour->PID);
+		int k = 0;
+		/* while (cour->command[k] != NULL) { */
+		printf("%s ", cour->command[k]);
+		/* 	++k; */
+		/* } */
+		printf("\n");
+		++j;
+		cour = cour->next;
+	    }
+	}
+    } else if (cl->seq[1]) {    /* Execute command line, command has pipe*/
+	if (cl->seq[2])
+	    fprintf(stderr,
+		    "error: execution of commands with multiple pipes "
+		    "not implemented\n");
+
+	pipe(fds);
+
+	/* Execute each command from command line */
+	switch (PID = fork()) {
+	case -1:
+	    /* there was an error during child creation */
+	    perror("fork:");
+	    break;
+	case 0:
+	{
+	    /* Process is child process, READING pipe end */
+	    dup2(fds[0], 0);
+	    close(fds[1]); close(fds[0]);
+
+	    if (out != 1) {
+		dup2(out, 1); 	/* Redirect output to out */
+		close(out);
+	    }
+
+	    execvp(cl->seq[1][0], cl->seq[1]);
+	}
+	default:
+	    break;
+	}
+    }
+
+    switch (PID = fork()) {
+    case -1:
+	/* there was an error during child creation */
+	perror("fork:");
+	break;
+    case 0:
+    {
+	/* Process is child process, WRITING pipe end */
+	if (cl->seq[1]) {
+	    dup2(fds[1], 1);
+	    close(fds[0]); close(fds[1]);
+	} else if (out != 1) {
+	    dup2(out, 1); 	/* Redirect output to out */
+	    close(out);
+	}
+
+	if (in != 0) {
+	    dup2(in, 0); 	/* Redirect input from in */
+	    close(in);
+	}
+
+	execvp(cl->seq[0][0], cl->seq[0]);
+    }
+    default:
+	/* Process is father and PID = its child's PID */
+#if DEBUG_PID
+	printf("child PID: %d\n", PID);
+#endif
+
+	if (cl->seq[1]) {
+	    close(fds[1]); close(fds[0]);
+	}
+
+	if (cl->bg) {
+	    printf("+ PID: %d\n", PID);
+
+	    shell_cmd *new = malloc(sizeof(shell_cmd));
+	    new->command = malloc(sizeof(char *));
+	    new->next = malloc(sizeof(shell_cmd));
+	    new->PID = PID;
+
+	    int k = 0;
+
+	    while (cl->seq[0][k] != NULL) {
+		strcpy(new->command[k], cl->seq[0][k]);
+		++k;
+	    }
+
+	    insert_shell_cmd(&BACKGROUND_PID, new);
+	}
+	else
+	    waitpid(PID, NULL, 0);
+
+	break;
+    }
+
+    return 0;
 }
 
 
@@ -150,155 +308,6 @@ int main() {
 	}
 #endif
 
-	/* Parse line */
-	struct cmdline *cl = parsecmd(&line);
-
-	/* If input stream closed, normal termination */
-	if (!cl) {
-	    terminate(0);
-	}
-
-	if (cl->err) {
-	    /* Syntax error, read another command */
-	    printf("error: %s\n", cl->err);
-	    return -1;
-	} else if (!cl->seq[0])	/* Empty line */
-	    return 0;
-
-#if DEBUG_COMMAND_LINE
-	if (cl->in) printf("in: %s\n", cl->in);
-	if (cl->out) printf("out: %s\n", cl->out);
-	if (cl->bg) printf("background (&)\n");
-
-	/* Display each command of the pipe */
-	for (int i = 0; cl->seq[i] != 0; i++) {
-	    char **cmd = cl->seq[i];
-	    printf("seq[%d]: ", i);
-	    for (int j = 0; cmd[j] != 0; j++) {
-		printf("'%s' ", cmd[j]);
-	    }
-	    printf("\n");
-	}
-#endif
-
-	int fds[2];			/* A pipe file-descriptor */
-	pid_t PID;
-
-	int in = 0;		/* input file desc: stdin by default */
-	if (cl->in)
-	    in = open(cl->in, O_RDONLY);
-	int out = 1;		/* output file desc: stdout by default */
-	if (cl->out)
-	    out = open(cl->out, O_WRONLY | O_CREAT);
-
-	/* If first command is "jobs" print PID */
-	if (!strncmp(cl->seq[0][0], "jobs", 4)) {
-	    if (BACKGROUND_PID.head == NULL)
-		printf("No background processes\n");
-	    else {
-		shell_cmd *cour = BACKGROUND_PID.head;
-		printf("Active background processes:\n");
-		int j = 1;
-		while (cour != NULL) {
-		    printf("+ [%d] PID: %d ", j,
-			   cour->PID);
-		    int k = 0;
-		    /* while (cour->command[k] != NULL) { */
-			printf("%s ", cour->command[k]);
-		    /* 	++k; */
-		    /* } */
-		    printf("\n");
-		    ++j;
-		    cour = cour->next;
-		}
-	    }
-	} else if (cl->seq[1]) {    /* Execute command line, command has pipe*/
-	    if (cl->seq[2])
-		fprintf(stderr,
-			"error: execution of commands with multiple pipes "
-			"not implemented\n");
-
-	    pipe(fds);
-
-	    /* Execute each command from command line */
-	    switch (PID = fork()) {
-	    case -1:
-		/* there was an error during child creation */
-		perror("fork:");
-		break;
-	    case 0:
-	    {
-		/* Process is child process, READING pipe end */
-		dup2(fds[0], 0);
-		close(fds[1]); close(fds[0]);
-
-		if (out != 1) {
-		    dup2(out, 1); 	/* Redirect output to out */
-		    close(out);
-		}
-
-		execvp(cl->seq[1][0], cl->seq[1]);
-	    }
-	    default:
-		break;
-	    }
-	}
-
-	switch (PID = fork()) {
-	case -1:
-	    /* there was an error during child creation */
-	    perror("fork:");
-	    break;
-	case 0:
-	{
-	    /* Process is child process, WRITING pipe end */
-	    if (cl->seq[1]) {
-		dup2(fds[1], 1);
-		close(fds[0]); close(fds[1]);
-	    } else if (out != 1) {
-		dup2(out, 1); 	/* Redirect output to out */
-		close(out);
-	    }
-
-	    if (in != 0) {
-		dup2(in, 0); 	/* Redirect input from in */
-		close(in);
-	    }
-
-	    execvp(cl->seq[0][0], cl->seq[0]);
-	}
-	default:
-	    /* Process is father and PID = its child's PID */
-#if DEBUG_PID
-	    printf("child PID: %d\n", PID);
-#endif
-
-	    if (cl->seq[1]) {
-		close(fds[1]); close(fds[0]);
-	    }
-
-	    if (cl->bg) {
-		printf("+ PID: %d\n", PID);
-
-		shell_cmd *new = malloc(sizeof(shell_cmd));
-		new->command = malloc(sizeof(char *));
-		new->next = malloc(sizeof(shell_cmd));
-		new->PID = PID;
-
-		int k = 0;
-
-		while (cl->seq[0][k] != NULL) {
-		    strcpy(new->command[k], cl->seq[0][k]);
-		    ++k;
-		}
-
-		insert_shell_cmd(&BACKGROUND_PID, new);
-	    }
-	    else
-		waitpid(PID, NULL, 0);
-
-	    break;
-	}
-
+	parse_and_execute_line(&line);
     }
 }
